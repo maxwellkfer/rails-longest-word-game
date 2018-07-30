e1d535cbb935caf3b02554528c5ac49111ced7aa
Maxime GUERIN <maxwellkfer@gmail.com>
2018-07-30 18:11:33 +0100

    longest word game last part missing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,12 +4,27 @@ class GamesController < ApplicationController
   end
 
   def score
-    word = params[:word]
-    @score =
-      if @letters.include?(word)
-        "yeah!"
-      else
-        "Nop..."
-      end
+    word = params[:word].upcase
+    letters = letters[:letters].split
+
+    @result
+    if all_letters_included?(letters, answer)
+      'Yeah!'
+    else
+      'Nop...'
+    end
+
+  end
+
+  private
+
+  def all_letters_included?(letter, word)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
